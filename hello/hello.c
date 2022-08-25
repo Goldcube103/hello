75ef47d3283a2c5e5731443864080445763e585e
Goldcube103 <walkerlee103@gmail.com>
2022-08-25 20:43:26 +0000

    initial commit

---
+++ hello/hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Walker Lee
+ * Email: wlee9@huskers.unl.edu
+ * Date: Aug 25, 2022
+ * Description:
+ * Outputs me and my partner's name and major
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+  printf("Walker Lee");
+
+}
